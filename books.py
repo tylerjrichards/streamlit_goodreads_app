a6f2aa9e34bb409195216ed21b3c94fc99e476ca
tyler.richards <tylerjrichards@gmail.com>
2023-10-29 20:19:30 -0700

    mito

--- books.py
+++ books.py
@@ -7,6 +7,7 @@ import plotly.express as px
 import requests
 import streamlit as st
 import xmltodict
+from mitosheet.streamlit.v1 import spreadsheet
 from pandas import json_normalize
 from streamlit_extras.add_vertical_space import add_vertical_space
 from streamlit_lottie import st_lottie
@@ -126,6 +127,11 @@ if has_records == False:
         "No books have been read in the past 5 years, so no analysis can be done. Try another profile!"
     )
     st.stop()
+
+
+with st.expander("Play around with your data with Mito!"):
+    data, code = spreadsheet(df)
+    st.code(code)
 
 st.write("")
 row3_space1, row3_1, row3_space2, row3_2, row3_space3 = st.columns(
@@ -411,7 +417,10 @@ with row7_1:
         )
     )
 
+
     st.markdown("***")
     st.markdown(
         "Thanks for going through this mini-analysis with me! I'd love feedback on this, so if you want to reach out you can find me on [twitter](https://twitter.com/tylerjrichards) or my [website](http://www.tylerjrichards.com/)."
     )
+
+
